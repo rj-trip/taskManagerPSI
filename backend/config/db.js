1f44c0eb6de25732458a0a0bcef3c9b292137954
rajlaxmi <rajlaxmit13@gmail.com>
2025-06-15 12:55:19 +0530

    code commited

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,8 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect("mongodb+srv://nishantjtiwari:902170%40MongoDB@cluster0.yoold.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-// //     mongoose.connect("mongodb+srv://nishantjtiwari:<902170%40MongoDB>@cluster0.yoold.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-// //    .then(() => console.log("✅ Connected to MongoDB"))
-// //    .catch(err => console.error("❌ MongoDB connection error:", err.message));
-//   } catch (err) {
-//     console.error("Mongo connection failed", err);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-     process.env.MONGODB_URI
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error("❌ Mongo connection failed:", err.message);
@@ -30,4 +11,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
